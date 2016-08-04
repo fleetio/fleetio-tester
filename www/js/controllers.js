ea23a464fac1370f47298826a48679a4eb0d1d2e
Jorge Valdivia <jvaldivia@rarestep.com>
2016-08-04 13:11:45 -0500

    use pure js for zd init

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,54 +2,54 @@ angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $timeout) {
 
-  var processStartOfDrive = function(zendriveDriveStartInfo) {
-    console.log("Detected trip start");
-    cordova.plugins.notification.local.schedule({
-      id: 1,
-      text: "Currently tracking your trip"
-    });
-  };
-  var processEndOfDrive = function(zendriveDriveInfo) {
-    console.log("Detected trip end");
-    cordova.plugins.notification.local.schedule({
-      id: 2,
-      text: "Trip has ended"
-    });
-  };
-  var processLocationDenied = function() {
-    console.log("Location denied, please enable location services to keep Zendrive working");
-  };
-
-  $scope.initZendrive = function(){
-    var applicationKey = "QBACZvxnFRs4eowTD6L5iGwrcOm1UANF";
-    var driverId = "4747";
-    var config = new Zendrive.ZendriveConfiguration(applicationKey, driverId);
-    var driverAttributes = new Zendrive.ZendriveDriverAttributes();
-
-    driverAttributes.email = "test-zendrive-sdk@fleetio.com";
-    driverAttributes.group = "18";
-    config.driverAttributes = driverAttributes;
-    config.driveDetectionMode = Zendrive.ZendriveDriveDetectionMode.ZendriveDriveDetectionModeAutoON;
-
-    var zendriveCallback = new Zendrive.ZendriveCallback(processStartOfDrive, processEndOfDrive, processLocationDenied);
-
-    Zendrive.setup(config, zendriveCallback,
-      function() {
-        alert("Zendrive Setup done!");
-      },
-      function (error) {
-        alert("Zendrive Setup failed: " + error);
-      }
-    )
-  }
-
-  $scope.$on("$ionicView.enter", function(event, data){
-    if(window.cordova) {
-      $timeout(function () {
-        $scope.initZendrive();
-      }, 5000);
-    }
-  });
+  // var processStartOfDrive = function(zendriveDriveStartInfo) {
+  //   console.log("Detected trip start");
+  //   cordova.plugins.notification.local.schedule({
+  //     id: 1,
+  //     text: "Currently tracking your trip"
+  //   });
+  // };
+  // var processEndOfDrive = function(zendriveDriveInfo) {
+  //   console.log("Detected trip end");
+  //   cordova.plugins.notification.local.schedule({
+  //     id: 2,
+  //     text: "Trip has ended"
+  //   });
+  // };
+  // var processLocationDenied = function() {
+  //   console.log("Location denied, please enable location services to keep Zendrive working");
+  // };
+  //
+  // $scope.initZendrive = function(){
+  //   var applicationKey = "QBACZvxnFRs4eowTD6L5iGwrcOm1UANF";
+  //   var driverId = "4747";
+  //   var config = new Zendrive.ZendriveConfiguration(applicationKey, driverId);
+  //   var driverAttributes = new Zendrive.ZendriveDriverAttributes();
+  //
+  //   driverAttributes.email = "test-zendrive-sdk@fleetio.com";
+  //   driverAttributes.group = "18";
+  //   config.driverAttributes = driverAttributes;
+  //   config.driveDetectionMode = Zendrive.ZendriveDriveDetectionMode.ZendriveDriveDetectionModeAutoON;
+  //
+  //   var zendriveCallback = new Zendrive.ZendriveCallback(processStartOfDrive, processEndOfDrive, processLocationDenied);
+  //
+  //   Zendrive.setup(config, zendriveCallback,
+  //     function() {
+  //       alert("Zendrive Setup done!");
+  //     },
+  //     function (error) {
+  //       alert("Zendrive Setup failed: " + error);
+  //     }
+  //   )
+  // }
+  //
+  // $scope.$on("$ionicView.enter", function(event, data){
+  //   if(window.cordova) {
+  //     $timeout(function () {
+  //       $scope.initZendrive();
+  //     }, 5000);
+  //   }
+  // });
 
 })
 
